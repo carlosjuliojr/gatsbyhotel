610496d0d766d586ad3957d8738557001cc02219
Julio <j.jaimes.rodriguez@gmail.com>
2020-07-24 18:41:18 -0400

    Gatsby Hotel, using react, gatsby datocms, graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,65 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React, { Fragment } from 'react';
+import Header from './header'
+import Footer from './footer'
+import Helmet from 'react-helmet' 
+import {Global, css } from '@emotion/core'
+import useSeo from '../hooks/use-seo'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+const Layout = (props) => {
+    const seo = useSeo();
+    const { fallbackSeo : {description, title}} = seo;
+	return (
+        <Fragment>
 
-import Header from "./header"
-import "./layout.css"
+            <Global
+                styles={css`
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+                    html {
+                        font-size:62.5%
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+                    body {
+                        font-size: 18px;
+                        font-size: 1.8rem;
+                        line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;;
+                    }
+                    h1, h2, h3  {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
 
-export default Layout
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                `}
+            />
+
+
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+            </Helmet>
+            <Header/>
+            {props.children}
+            <Footer title={title} />
+        </Fragment>
+    );
+};
+
+export default Layout;
